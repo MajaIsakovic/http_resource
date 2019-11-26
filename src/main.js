15f71ecbebce23fe441984f985f81deb0c9e5a55
Maja Isakovic <majamaja733@hotmail.com>
2019-11-26 10:18:36 +0100

    Vue resource file location import changed

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-import VueResource from './App.vue';
+import VueResource from "vue-resource"
 
 Vue.config.productionTip = false
-
 //extend core functionality adding resource to use function
 Vue.use(VueResource);
 
