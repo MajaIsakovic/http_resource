240b5935569ef0da56c26f611acaeb754815a3cf
Maja Isakovic <majamaja733@hotmail.com>
2019-11-26 09:28:13 +0100

    registering vue resource to main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueResource from './App.vue';
 
 Vue.config.productionTip = false
+
+//extend core functionality adding resource to use function
+Vue.use(VueResource);
 
 new Vue({
   render: h => h(App),
